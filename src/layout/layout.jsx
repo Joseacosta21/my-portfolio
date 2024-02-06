f40212d24975e276c8d5fe7804ac5a897915d878
Joseacosta21 <joseacostaaldrete@gmail.com>
2024-02-06 17:34:28 -0500

    General Updates

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -6,10 +6,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Header />
-      <main className="content">
-        {children}
-        <p>Verga Aguad</p>
-      </main>
+      <main className="content">{children}</main>
     </div>
   );
 };
