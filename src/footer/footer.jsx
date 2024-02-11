8421936cad84efdca3284003adb51f9d1a06132e
Joseacosta21 <joseacostaaldrete@gmail.com>
2024-02-10 19:37:36 -0500

    General Updates

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -1,56 +1,53 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 import "./footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faWhatsapp,
+  faLinkedin,
+  faYoutube,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 const Footer = () => {
   return (
     <>
       <div className="footer-container">
-        <div className="body">
-          <div className="column">
-            <ul className="footerLink">
-              <SocialIcon
-                className="social-media-icon"
-                fgColor="currentColor"
-                target="_blank"
-                url="https://github.com/Joseacosta21"
-              />
-              <SocialIcon
-                className="social-media-icon"
-                fgColor="currentColor"
-                target="_blank"
-                url="https://wa.me/524441737109"
-              />
-              <SocialIcon
-                className="social-media-icon"
-                fgColor="currentColor"
-                target="_blank"
-                url="mailto:joseacostaaldrete@gmail.com"
-              />
-            </ul>
-          </div>
-          <div className="column">
-            <ul className="footerLink">
-              <SocialIcon
-                className="social-media-icon"
-                fgColor="currentColor"
-                target="_blank"
-                url="https://www.linkedin.com/in/joseacostaaldrete/"
-              />
-              <SocialIcon
-                className="social-media-icon"
-                fgColor="currentColor"
-                target="_blank"
-                url="https://www.youtube.com/channel/UCEK1R1HfFmtuCfA9Bcmeu7w"
-              />
-              <SocialIcon
-                className="social-media-icon"
-                fgColor="currentColor"
-                target="_blank"
-                url="https://instagram.com/jacosta_foto"
-              />
-            </ul>
-          </div>
+        <div className="column">
+          <ul className="footerLink">
+            <li>
+              <a href="https://github.com/Joseacosta21">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            </li>
+            <li>
+              <a href="https://wa.me/524441737109">
+                <FontAwesomeIcon icon={faWhatsapp} />
+              </a>
+            </li>
+            <li>
+              <a href="mailto:joseacostaaldrete@gmail.com">
+                <FontAwesomeIcon icon={faEnvelope} />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/joseacostaaldrete/">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/channel/UCEK1R1HfFmtuCfA9Bcmeu7w">
+                <FontAwesomeIcon icon={faYoutube} />
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com/jacosta_foto">
+                <FontAwesomeIcon icon={faInstagram} />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </>
