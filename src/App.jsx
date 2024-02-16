cc541c4e6362b8d8ad9d71985ab5592200a98a03
Joseacosta21 <joseacostaaldrete@gmail.com>
2024-02-15 23:37:53 -0500

    Favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Layout from "./layout/layout";
-import { BrowserRouter as Router, NavLink } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
   return (
     <Router className="router">
-
       <div className="layout-container">
         <Layout />
       </div>
