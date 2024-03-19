e65878a273139f20001e02df551d13696a36a178
Joseacosta21 <joseacostaaldrete@gmail.com>
2024-03-19 13:21:33 -0400

    Engineering Container

--- src/engineering/engineering.jsx
+++ src/engineering/engineering.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./engineering.css";
 import ProjectsCard from "../ProjectsCard/ProjectsCard";
 import { motion, useTransform, useScroll } from "framer-motion";
-import { useRef } from "react";
+//import { useRef } from "react";
 
 const Engineering = () => {
   // Project Cards
@@ -17,37 +17,34 @@ const Engineering = () => {
   };
 
   // Scrolling Goober
-  const targetRef = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: targetRef,
-  });
+  //const targetRef = useRef(null);
+  //const { scrollYProgress } = useScroll({
+  //  target: targetRef,
+  //});
 
-  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-30%"]);
+  //const x = useTransform(scrollYProgress, [0, 1], ["1%", "-30%"]);
 
   return (
     <>
       <div
-        className="engineering-container topic relative h-[300vh]"
+        className="engineering-container topic content-center"
         id="engineering"
-        ref={targetRef}
       >
-        <section className="sticky top-0 flex justify-center h-screen items-center  overflow-hidden  pt-12">
-          <motion.div style={{ x }} className="flex gap-1">
-            <div className="text-center mr-4 p-10">
-              <h1 className="title text-center">Engineering</h1>
-              <br />
-              <p>
-                Currently studying B.Eng. Mechanical Engineering at Concordia
-                University.
-              </p>
-              <br />
-              <p>The brown puffle from Club Penguin is my spirit animal.</p>
-              <br />
-              <p>Some engineering projects ive made:</p>
-            </div>
-            <ProjectsCard {...pillDispenser} />
-          </motion.div>
-        </section>
+        <div className="column-container self-center items-center">
+          <div className="text-center mr-4 p-10">
+            <h1 className="title text-center">Engineering</h1>
+            <br />
+            <p>
+              I am a B.Eng. Mechanical Engineering Co-OpStudent in at Concordia
+              University.
+            </p>
+            <br />
+            <p>The brown puffle from Club Penguin is my spirit animal.</p>
+            <br />
+            <p>Some of my engineering curriculum:</p>
+          </div>
+          <ProjectsCard {...pillDispenser} />
+        </div>
       </div>
     </>
   );
