61b0a22aa94bc04f56d856ade1a2c799b9264430
Joseacosta21 <joseacostaaldrete@gmail.com>
2025-01-07 00:12:55 -0600

    Updateees people updates

--- src/music/music.jsx
+++ src/music/music.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import "./music.css";
 import ProjectsCard from "../ProjectsCard/ProjectsCard";
-import { motion, useTransform, useScroll } from "framer-motion";
-import { useRef } from "react";
+import "./music.css";
 
 const Music = () => {
-  // Project Cards
   const JazzHouse = {
     projectImg: "/assets/project-card-photos/music/JazzHouse.png",
     projectTitle: "Jazz House DJ Set",
@@ -30,7 +27,6 @@ const Music = () => {
     projectGitHub: "",
     projectLink: "",
   };
-
   const AfroHouse = {
     projectImg: "/assets/project-card-photos/music/AfroHouse.jpg",
     projectTitle: "Afro House DJ Set",
@@ -39,7 +35,6 @@ const Music = () => {
     projectGitHub: "",
     projectLink: "https://youtu.be/2_-RP0J6uPE?si=RhmgFNM7i8CyA8m5",
   };
-
   const EnjoytheSilence = {
     projectImg: "/assets/project-card-photos/music/EnjoytheSilence.jpg",
     projectTitle: "Enjoy the Silence - Dirty Hands Mix",
@@ -50,28 +45,17 @@ const Music = () => {
     projectLink: "https://youtu.be/Sejj8WNgzwM?si=RQUoer9u-afScBOO",
   };
 
-  // Scrolling Goober
-  const targetRef = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: targetRef,
-  });
-
-  const x = useTransform(scrollYProgress, [0, 1], ["1%", "400%"]);
-
   return (
     <>
-      <div
-        className="music-container topic relative h-[300vh] px-0 flex-col"
-        id="music"
-        ref={targetRef}
-      >
-        <section className="sticky top-0 flex justify-center h-screen items-center overflow-hidden">
-          <motion.div style={{ x }} className="flex gap-1">
-            <div className="text-center">
+      <div className="music-container content-center" id="music">
+        <div className="projects-container">
+          {/*Still dont know why this div is needed */}
+          <div className="flex gap-1">
+            {/* Text */}
+            <div className="text-container">
               <h1 className="title">Music</h1>
               <p>Matching BPMs and heart rates</p>
               <br />
-
               <div className="skills">
                 <h3>Software Skills</h3>
                 <ul>
@@ -90,23 +74,11 @@ const Music = () => {
                 </ul>
               </div>
             </div>
-          </motion.div>
-        </section>
-        <br />
-        <div className="projects-container">
-          <ProjectsCard {...TimeoutMarket} />
-          <ProjectsCard {...FanaBox} />
-          <ProjectsCard {...JazzHouse} />
-          <ProjectsCard {...AfroHouse} />
-          <ProjectsCard {...EnjoytheSilence} />
-          <div>
-            <iframe
-              src="https://open.spotify.com/embed/playlist/4E4oSQkL1597H572J4LZQb?utm_source=generator"
-              width="100%"
-              height="100%"
-              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-              loading="lazy"
-            ></iframe>
+            <ProjectsCard {...TimeoutMarket} />
+            <ProjectsCard {...FanaBox} />
+            <ProjectsCard {...JazzHouse} />
+            <ProjectsCard {...AfroHouse} />
+            <ProjectsCard {...EnjoytheSilence} />
           </div>
         </div>
       </div>
